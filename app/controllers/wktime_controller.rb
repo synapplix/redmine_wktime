87b512fa85b5870fa48a060036288f4a5575ad60
Wolfgang Braeu <wbraeu@synapplix.de>
2014-07-02 19:20:20 +0200

    Sorting entries according to their creation time

--- app/controllers/wktime_controller.rb
+++ app/controllers/wktime_controller.rb
@@ -1229,9 +1229,10 @@ private
 		@wktimes = Wktime.find(:all, :conditions => cond)
 	end
 	
+	#Entries are now sorted depending on their creation time
 	def findEntriesByCond(cond)
 		TimeEntry.find(:all, :conditions => cond,
-			:order => 'project_id, issue_id, activity_id, spent_on')
+			:order => 'created_on, project_id, issue_id, activity_id, spent_on')
 	end
 	
 	def setValueForSpField(teEntry,spValue,decimal_separator,entry)
