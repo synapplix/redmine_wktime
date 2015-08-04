52435c8434d6936325f987b0fdcd1308c97b7a78
wbraeu <wbraeu@synapplix.de>
2015-08-04 17:50:23 +0200

    Added ':checkbox' to the list of input-types which will be ignored when being changed

--- assets/javascripts/alert_unsaved_changes.js
+++ assets/javascripts/alert_unsaved_changes.js
@@ -2,7 +2,7 @@ var warnMessage = "You have unsaved changes!";
 
 $(document).ready(function () {	
 	
-		$('#wktime_edit input:not(:submit), #wktime_edit select, #comment-dlg textarea, #comment-dlg select, .ui-dialog textarea, .ui-dialog select').change(function()  { 
+		$('#wktime_edit input:not(:submit, :checkbox), #wktime_edit select, #comment-dlg textarea, #comment-dlg select, .ui-dialog textarea, .ui-dialog select').change(function()  {
 			//triggers change in all input fields including text type
 			// #comment-dlg textarea, #comment-dlg select are the spectific fields in the popup showComment
     		window.onbeforeunload = function () {
